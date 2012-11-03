29b9d493c34e9c7100721c4f0fbdbcab5c1e9e06
Christopher Tiee <choni0281@gmail.com>
2012-11-02 21:55:22 -0700

    Initial Commit

---
+++ big_int_new/big_int_newPriv.h
@@ -0,0 +1,19 @@
+/*
+ *  big_int_newPriv.h
+ *  big_int_new
+ *
+ *  Created by Chris on 11/2/12.
+ *  Copyright (c) 2012 Chris. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class big_int_newPriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
